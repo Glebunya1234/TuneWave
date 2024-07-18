7a7dd7ad2885d4d987b263176e1245d3e31ccdee
Glebunya1234 <glebbuckiy@gmail.com>
2024-07-18 22:25:58 +0800

    Create new app and tasting scss

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,10 @@
+import styles from "@/app/profile/profile.module.scss";
+const Profile = () => {
+  return (
+    <div className={styles.profile}>
+      <p>Hello Wold</p>
+    </div>
+  );
+};
+
+export default Profile;
