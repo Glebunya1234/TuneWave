16beb5b8db07965f9aa24032a4d1b2a8a3683a15
Glebunya1234 <glebbuckiy@gmail.com>
2024-07-18 22:25:58 +0800

    Create new app and tasting scss

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,10 @@
+import styles from "@/app/auth/auth.module.scss";
+const Profile = () => {
+  return (
+    <div className={styles.profile}>
+      <p>Hello Wold</p>
+    </div>
+  );
+};
+
+export default Profile;
