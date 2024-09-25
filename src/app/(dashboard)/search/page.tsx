8f3655bc5aee201075c5ee3b71796d0fe6a86599
Glebunya1234 <glebbuckiy@gmail.com>
2024-09-25 21:09:41 +0800

    added a "random phrase" component to the main search page

--- src/app/(dashboard)/search/page.tsx
+++ src/app/(dashboard)/search/page.tsx
@@ -1,10 +1,12 @@
 import { SearchTegsBtn } from "@/components/UI/Buttons/SearchTegsBtn/SearchTegsBtn";
 import style from "./search.module.scss";
+import { RandomPhrase } from "@/components/UI/WelcomePhrases-Component/WelcomePhrases";
 
 const Search = ({ params }: { params: { id: string } }) => {
   return (
     <aside className={style.Search}>
-      <p>Main</p>
+      {/* <p>Main</p> */}
+      <RandomPhrase />
     </aside>
   );
 };
