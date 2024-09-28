f77f772b9c0a0e07fb55350079dac855e673aec1
Glebunya1234 <glebbuckiy@gmail.com>
2024-09-28 19:23:04 +0800

    description of the code with comments for the context of components, files

--- src/components/Context-component/Context.tsx
+++ src/components/Context-component/Context.tsx
@@ -1,35 +1,52 @@
 "use client";
-
 import { useState } from "react";
 import { GlobalContext } from "@/Context";
 import { usePathname } from "next/navigation";
 import { ContextType } from "@/types/TypeContext/type";
+
+//главный конетекст компонент что оборачивает
 export const Context = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
   const text = pathname?.split("/").pop() || "tunewave";
+
   const [getTemporaryText, setTemporaryText] =
     useState<ContextType["getTemporaryText"]>(text);
+  //текущий текст заднего фона
+
   const [getDefaultText, setDefaultText] =
     useState<ContextType["getDefaultText"]>(text);
+  //дефолтный текст заднего фона, может изменятся
   const [isHiddenLeftPhoneBar, setHiddenLeftPhoneBar] =
     useState<ContextType["isHiddenLeftPhoneBar"]>(false);
   const [isHiddenLeftBar, setHiddenLeftBar] =
     useState<ContextType["isHiddenLeftBar"]>(true);
+  //Состояние левого сайд бара (телефон, пк)
+
   const [isHiddenRightBar, setHiddenRightBar] =
     useState<ContextType["isHiddenRightBar"]>(true);
+  //Состояние правого сайд бара
+
+  const [isRemoveLeft, setRemoveLeft] =
+    useState<ContextType["isRemoveLeft"]>(true);
+  const [isRemovePhoneLeft, setRemovePhoneLeft] =
+    useState<ContextType["isRemovePhoneLeft"]>(false);
+  //Состояние для вызивания анимации удаления для левого сайд бара (телефон, пк)
+
+  const [isRemoveRight, setRemoveRight] =
+    useState<ContextType["isRemoveRight"]>(true);
+  //Состояние для вызивания анимации удаления для правого сайд бара пк
+
+  //Плеер
   const [getStatePlaying, setStatePlaying] = useState<
     ContextType["getStatePlaying"]
   >({ progress_ms: 0, is_playing: false, duration_ms: 0 });
   const [getCurrentPlaying, setCurrentPlaying] =
     useState<ContextType["getCurrentPlaying"]>();
+
   const [getDeviceID, setDeviceID] = useState<ContextType["getDeviceID"]>("");
+
   const [player, setPlayer] = useState<ContextType["player"]>(null);
-  const [isRemoveLeft, setRemoveLeft] =
-    useState<ContextType["isRemoveLeft"]>(true);
-  const [isRemovePhoneLeft, setRemovePhoneLeft] =
-    useState<ContextType["isRemovePhoneLeft"]>(false);
-  const [isRemoveRight, setRemoveRight] =
-    useState<ContextType["isRemoveRight"]>(true);
+
   const valueHover = {
     getTemporaryText,
     setTemporaryText,
--- src/types/TypeContext/type.ts
+++ src/types/TypeContext/type.ts
@@ -9,20 +9,32 @@ export type ContextType = {
 
     getTemporaryText: string
     setTemporaryText: TypeSetState<string>;
+    //текущий текст заднего фона
     getDefaultText: string
     setDefaultText: TypeSetState<string>;
+    //дефолтный текст заднего фона, может изменятся
+
     isHiddenLeftBar: boolean,
     setHiddenLeftBar: TypeSetState<boolean>;
     isHiddenLeftPhoneBar: boolean,
     setHiddenLeftPhoneBar: TypeSetState<boolean>;
+    //Состояние левого сайд бара (телефон, пк)
+
     isHiddenRightBar: boolean,
     setHiddenRightBar: TypeSetState<boolean>;
+    //Состояние правого сайд бара
+
     isRemovePhoneLeft: boolean
     setRemovePhoneLeft: TypeSetState<boolean>;
     isRemoveLeft: boolean
     setRemoveLeft: TypeSetState<boolean>;
+    //Состояние для вызивания анимации удаления для левого сайд бара (телефон, пк)
+
     isRemoveRight: boolean
     setRemoveRight: TypeSetState<boolean>;
+    //Состояние для вызивания анимации удаления для правого сайд бара пк
+
+    //Плеер
     getStatePlaying: state,
     setStatePlaying: TypeSetState<state>;
     getCurrentPlaying: any,
