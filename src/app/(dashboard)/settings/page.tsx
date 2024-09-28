317e47e72df5168144480e713e595f25f5e95daa
Glebunya1234 <glebbuckiy@gmail.com>
2024-09-28 21:23:42 +0800

    settings page

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -1,16 +1,59 @@
-import { PanelTarget } from "@/components/UI/Target/PanelTarget";
+"use server";
 import style from "./Settings.module.scss";
 import { SignOutBTN } from "@/components/UI/Buttons/AuthBtn/SignOut-button/SignOutBtn";
-
-const Settings = () => {
+import { OpenInSpotify } from "@/components/UI/Buttons/OpenInSpotifyBtn/OpenInSpotify";
+import { GetDataProfileUser } from "@/providers/SupaBase-methods/user-action";
+import { getDataCacheUser } from "@/utils/helper/CacheHelper/cacheHLP";
+import Image from "next/image";
+import { FaCircle } from "react-icons/fa";
+const Settings = async () => {
+  const userData = await getDataCacheUser();
+  const userDataSupabase = await GetDataProfileUser();
   return (
     <div className={style.Settings}>
-      <section className={style.Settings__Dashboard}>
-        <aside className={style.Settings__Content}>
-          <SignOutBTN />
-          <div>Settings</div>
-        </aside>
-      </section>
+      <aside className={style.Settings_Content}>
+        <section className={style.Content__SectionImage}>
+          <div className={style.ImageStyles}>
+            <Image
+              src={
+                userData?.images[1].url === undefined
+                  ? ""
+                  : userData?.images[1].url
+              }
+              layout="fill"
+              objectFit="cover"
+              className={` ${style.mark}`}
+              alt=""
+            />
+          </div>
+          <span>
+            <h2>Status</h2>
+            <h3>
+              <FaCircle className="ml-3 mr-1 text-green-400 " />
+              Online
+            </h3>
+          </span>
+        </section>
+        <section className={style.Content__SectionUserData}>
+          <div className={style.UserData__DataConteiner}>
+            <p>Username</p>
+            <h2>{userData?.display_name}</h2>
+            <div className="mt-[8px] w-full border-[#ffffffaf] border-[1px] " />
+            <p>Email</p>
+            <h2>{userDataSupabase.user?.email}</h2>
+            <div className="mt-[8px] w-full border-[#ffffffaf] border-[1px] " />
+            <p>Open a profile in the Spotify app</p>
+            <OpenInSpotify
+              href={`${userData.external_urls.spotify}`}
+              text="Open in Spotify"
+              className={style.OpenSpotifyBtn}
+            />
+            <div className="mt-[8px] w-full border-[#ffffffaf] border-[1px] " />
+            <p>Log out of your account</p>
+            <SignOutBTN />
+          </div>
+        </section>
+      </aside>
     </div>
   );
 };
--- src/utils/helper/CacheHelper/cacheHLP.ts
+++ src/utils/helper/CacheHelper/cacheHLP.ts
@@ -1,4 +1,6 @@
 import { _getArtists } from "@/api/SP-Artists/API-SP-Artists";
+import { GetUser } from "@/api/SP-Users/API-SP-Users";
+import { CurrentlyUserType } from "@/types/SpotifyTypes/CurrentlyUser/type";
 import { TrackArtist } from "@/types/SpotifyTypes/TrackArtist/type";
 import { cache } from "react";
 
@@ -26,6 +28,36 @@ export const getDataCacheArtist = cache(async (id: string) => {
             popularity: "",
             type: "",
             uri: "",
+        }
+    }
+});
+
+export const getDataCacheUser = cache(async () => {
+    try {
+        const userData: CurrentlyUserType = await GetUser();
+        return userData;
+    }
+    catch {
+        return {
+            display_name: '',
+            country: '',
+            external_urls: {
+                spotify: '',
+            },
+            followers: {
+                href: '',
+                total: 0,
+            },
+            href: '',
+            id: '',
+            images: [
+                {
+                    url: '',
+                }
+            ],
+            type: '',
+            product: '',
+            uri: '',
         }
     }
 });
